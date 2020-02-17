35537040f088f6d539aa66ecaef76754d29dc24d
Mat Anderson <matandersonlv@hotmail.com>
2020-02-17 22:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ class Bird {
   }
   
   speak() {
-    if (sex == 'male') {
+    if (this.sex == 'male') {
       return `It's me! ${this.name}`
     } else {
       return `${this.name} says squawk!`
